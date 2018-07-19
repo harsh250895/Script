d3e3f417b389204949ab8882086d52db622246e1
harsh250895 <harsh250895@gmail.com>
2018-07-19 00:14:38 -0400

    final

--- test.py
+++ test.py
@@ -23,19 +23,19 @@ def tcpList():
 #4: Kill all sleeping processes.
 #p.s: created a log file to store a list of all sleeping process, killing all of them resulted in a system reboot for unexplained reasons. Hence, for debugging purposes. 
 def killSleepingProc():
-    CMD = "ps h -eo s,ppid | awk '{ if ($1 == \"S\" || $1 == \"D\") { print $2 } }' > sleep.log"
+    CMD = "ps h -eo s,ppid,comm | awk '{ if ($1 == \"S\" || $1 == \"D\") { print $2 } }' > sleep.log"
     output = subprocess.check_output(CMD, shell=True)
-    t = output.split('\n')
-    for each in t:
-        if each.isdigit() == True:
-            print(each)
-            m = "sudo kill -9 "+each
-#            os.system(m)
+#   t = output.split('\n')
+#   for each in t:
+#       if each.isdigit() == True:
+#           print(each)
+#           m = "sudo kill -9 "+each
+#           os.system(m)
 
 #5: Checing if any processes are in sleep state
 def sleepCheck():
      print("Checking for sleeping processes...")
-     cmd = "ps -e -o s | grep -o 'S\|D' -c "
+     cmd = "ps -eo s | grep -o 'S\|D' -c "
      num = os.popen(cmd)
      outp = num.read()
      if outp > 0:
@@ -43,17 +43,19 @@ def sleepCheck():
         sys.exit(1)
 
 t = strftime("%H:%M", gmtime())
-list = ["01:00","02:00","03:00","04:00","05:00","06:00","07:00","08:00","09:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00","23:00","24:00"]
+list = ["01:00","02:00","03:00","04:00","05:00","06:00","07:00","08:00","09:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00","23:00","00:00"]
 
-#Call functions if time is on the hour
-for each in list:
-    if each == "01:00":
-        seconds = input("Enter the number of seconds: ")
-        backgroundSleep(seconds)
-        max_mem_cpu()
-        tcpList()
-        killSleepingProc()
-        sleepCheck()
+#Call functions if time is on the hour every 10 seconds
+while True:
+    for each in list:
+        if each == t:
+            seconds = input("Enter the number of seconds: ")
+            backgroundSleep(seconds)
+            max_mem_cpu()
+            tcpList()
+            killSleepingProc()
+            sleepCheck()
+    os.system("sleep 10")
 
 
 
